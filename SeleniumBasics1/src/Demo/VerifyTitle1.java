64665b2b6618c93d9ae9007887ed12e56f351815
Parimudmal <shreya999@gmail.com>
2019-10-03 10:00:34 -0500

    adding files from desktop github_local to server repository

---
+++ SeleniumBasics1/src/Demo/VerifyTitle1.java
@@ -0,0 +1,9 @@
+package Demo;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class VerifyTitle1 {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+WebDriver driver = new ChromeDriver();
+}
+}
